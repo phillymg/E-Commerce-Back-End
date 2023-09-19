c2e81c4d42028fe77a0212777eb7c3073fd0d9f3
Philmg <phillipmguetterman@att.net>
2023-09-18 22:03:04 -0500

    swap product_id and tag_id in index.js, fixed issue

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -40,30 +40,23 @@ router.post('/', async (req, res) => {
   */
   try {
     const productData = await Product.create(req.body);
+
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: productData.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-Product.create(req.body)
-  .then((product) => {
-    if (req.body.tagIds.length) {
-      const productTagIdArr = req.body.tagIds.map((tag_id) => {
-        return {
-          product_id: product.id,
-          tag_id,
-        };
-      });
-      return ProductTag.bulkCreate(productTagIdArr);
-    }
-    res.status(200).json(product);
-  })
-  .then((productTagIds) => res.status(200).json(productTagIds))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
 
 router.put('/:id', (req, res) => {
   Product.update(req.body, {
